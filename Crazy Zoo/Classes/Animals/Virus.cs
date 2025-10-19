a616f886707b102b2c3aca201bb6b4329084fce4
maggieeagle <oryol.margo@gmail.com>
2025-10-19 04:34:49 -0700

    added classes Snail, Zebra and Virus

---
+++ Crazy Zoo/Classes/Animals/Virus.cs
@@ -0,0 +1,41 @@
+﻿using Crazy_Zoo.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crazy_Zoo.Classes.Animals
+{
+    internal class Virus : BaseAnimal, ICrazy, IRunnable
+    {
+        public Virus(string name = "Unknown", string species = "Virus", string voice = "...", string introduction = "") : base(name, species, voice, introduction = $"{name} waiting for his hour to conquer the world")
+        {
+        }
+
+        public override int Action()
+        {
+            return CrazyAction();
+        }
+
+        public int CrazyAction()
+        {
+            return 2;
+        }
+
+        public override string EatFood(string food)
+        {
+            return $"Such powerful creature as {this.GetName()} doesn't need any {food}";
+        }
+
+        public override string MakeSound()
+        {
+            return GetVoice();
+        }
+
+        public string Run()
+        {
+            return $"{this.GetName()} moves in microscopic scale";
+        }
+    }
+}
